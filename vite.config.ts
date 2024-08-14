9ad62eda44edd5cf604ff0e4b585a76d6ba03cba
João Vitor <jvitordiass@outlook.com>
2024-08-14 09:03:05 -0300

    chore(boilerplate): initial commit
      create project boilerplate from https://github.com/diasjoaovitor/vite-boilerplate

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
