733e811a5d4a1357156856efe767ce48b91b490c
João Vitor <jvitordiass@outlook.com>
2024-08-22 17:46:08 -0300

    chore: disable "--max-warnings 0" in lint

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -4,7 +4,7 @@ const buildCommand = (filenames) => {
   const files = filenames.map((f) => path.relative(process.cwd(), f))
   return [
     `npx prettier --write ${files.join(' --file ')}`,
-    `npx eslint --fix ${files.join(' ')} --report-unused-disable-directives --max-warnings 0`,
+    `npx eslint --fix ${files.join(' ')} --report-unused-disable-directives`,
     `npx vitest related ${files.join(' ')} --passWithNoTests --no-file-parallelism --run`
   ]
 }
